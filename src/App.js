47db619643556133966574539234b2d772f7861a
Vereselen <rodion_312@mail.ru>
2024-12-19 06:46:00 +0300

    check

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Скрынникова Р.В.";
+  const greeting = "Это тестовый проект";
+  const greeting_1 = "Это тестовый проект";
 
-  
   return (
     <div className="App">
      <header className="App-header">
